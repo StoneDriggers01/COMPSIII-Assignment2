f3b067c8e946af0dcf4bc45f4bfec5a00b54c552
StoneDriggers01 <stonedriggersofficial@gmail.com>
2025-09-14 23:52:13 -0400

    stuff

--- person.py
+++ person.py
@@ -1,3 +1,17 @@
 class Person:
-    # Delete pass and write your code here
     pass
+    def __init__(self, name, age, country):
+        self.name = name
+        self.age = age
+        self.country = country
+    
+    def __str__(self):
+        return f"{self.name} is {self.age} years old and is from {self.country}."
+
+    
+Per_1 = Person('John', 17, 'Canada')
+Per_2 = Person('Maria', 25, 'Spain')
+
+print(Per_1)
+print(Per_2)
+
